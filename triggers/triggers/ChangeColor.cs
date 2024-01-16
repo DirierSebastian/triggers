f1920b674377ffa14b182ba46fc657679f9ea331
DirierSebastian <sebastianagvzla@gmail.com>
2024-01-16 08:58:52 -0700

    Agregar archivos de proyecto.

---
+++ triggers/triggers/ChangeColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace triggers
+{
+    public class ChangeColor : TriggerAction<Button>
+    {
+        protected override void Invoke(Button btn)
+        {
+            btn.BackgroundColor = Color.SkyBlue;
+            btn.Text = "DreamTeam";
+        }
+    }
+}
